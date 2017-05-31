f66d3ce16e2c8a6dcf8370a0d8ac858281eb729c
Gerardo Saca <gsacavdm@microsoft.com>
2017-05-30 21:27:52 -0700

    Factor out Tenant in UserDetailsClient\App.cs

--- UserDetailsClient/UserDetailsClient/App.cs
+++ UserDetailsClient/UserDetailsClient/App.cs
@@ -13,6 +13,7 @@ namespace UserDetailsClient
         public static PublicClientApplication PCA = null;
         
         // Azure AD B2C Coordinates
+        public static string Tenant = "fabrikamb2c.onmicrosoft.com";
         public static string ClientID = "90c0fe63-bcf2-44d5-8fb7-b8bbc0b29dc6";
         public static string PolicySignUpSignIn = "b2c_1_susi";
         public static string PolicyEditProfile = "b2c_1_edit_profile";
@@ -21,7 +22,7 @@ namespace UserDetailsClient
         public static string[] Scopes = { "https://fabrikamb2c.onmicrosoft.com/demoapi/demo.read" };
         public static string ApiEndpoint = "https://fabrikamb2chello.azurewebsites.net/hello";
 
-        public static string AuthorityBase = "https://login.microsoftonline.com/tfp/fabrikamb2c.onmicrosoft.com/";
+        public static string AuthorityBase = $"https://login.microsoftonline.com/tfp/{Tenant}/";
         public static string Authority = $"{AuthorityBase}{PolicySignUpSignIn}";
         public static string AuthorityEditProfile = $"{AuthorityBase}{PolicyEditProfile}";
         public static string AuthorityPasswordReset = $"{AuthorityBase}{PolicyResetPassword}";
