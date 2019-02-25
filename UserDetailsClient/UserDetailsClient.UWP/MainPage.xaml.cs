e4b382f2b18cd187e434ffa6fea009b91bc44d81
Jenny Ferries <jeferrie@microsoft.com>
2019-02-25 09:31:03 -0800

    remove uwp app callback

--- UserDetailsClient/UserDetailsClient.UWP/MainPage.xaml.cs
+++ UserDetailsClient/UserDetailsClient.UWP/MainPage.xaml.cs
@@ -23,7 +23,6 @@ namespace UserDetailsClient.UWP
             this.InitializeComponent();
 
             var app = new UserDetailsClient.Core.App();
-            UserDetailsClient.Core.App.PCA.RedirectUri = WebAuthenticationBroker.GetCurrentApplicationCallbackUri().ToString();
             LoadApplication(app);
         }
     }
